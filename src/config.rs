379998a7fe670cc70a25b7a06678fd7bd38294a8
RozeFound <rozefound@proton.me>
2024-07-18 18:14:04 +0700

    Make it run

---
+++ src/config.rs
@@ -0,0 +1,4 @@
+pub static VERSION: &str = "0.0.0-dev";
+pub static GETTEXT_PACKAGE: &str = "suraimu";
+pub static LOCALEDIR: &str = "/app/share/locale";
+pub static PKGDATADIR: &str = "/app/share/suraimu";
--- suraimu/config.py
+++
@@ -1,9 +0,0 @@
-APP_ID = "@APP_ID@".removesuffix('.Devel')
-APP_VERSION = "@APP_VERSION@"
-APP_NAME = "@APP_NAME@"
-
-APP_NAME_LOWER = APP_NAME.lower()
-RESOURCES = '/' + APP_ID.removesuffix('.Devel').replace('.', '/')
-APP_ICON = "@APP_ID@"
-
-BUILD_TYPE = "@BUILD_TYPE@"
